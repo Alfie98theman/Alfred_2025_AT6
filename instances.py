611b6e35dea050e1bd41a742ed9028d11dc9e3b3
Alfie98theman <alfredscriven26@gmail.com>
2025-11-14 19:18:16 +0100

    Commit 3: Update the second Python file to call the validation method on both instances and display results.

--- instances.py
+++ instances.py
@@ -2,3 +2,11 @@ from UserInputValidator import UserInputValidator
 
 first_validator = UserInputValidator()
 second_validator = UserInputValidator()
+
+list1_of_strings = ["13", "abc", "-13", "1000"]
+list2_of_strings = ["45", "hello world", "-12.2", "1"]
+
+print(" The first Validator's results:")
+first_validator.validation_message(list1_of_strings)
+print("\n The second Validator's results:")
+second_validator.validation_message(list2_of_strings)
